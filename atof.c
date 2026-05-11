f729c60b557f0d882c90fa8a1061a36129487a2b
devoltamp <devparmar2306@gmail.com>
2026-05-11 22:58:56 +0530

    str to double

---
+++ atof.c
@@ -0,0 +1,11 @@
+// making atoi via the use of atof
+// or could have made .h 
+#include <stdio.h>
+
+int atoi(char s[]){
+
+    double atof(char s[]);
+    int temp = (int) atof(s);
+
+    return temp
+}
