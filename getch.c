e69bd3b46268464c63ccc09d8e52fbcdc926923a
devoltamp <devparmar2306@gmail.com>
2026-05-19 23:50:14 +0530

    updated the code but still how it gets connected to bufp is a problem

--- getch.c
+++ getch.c
@@ -1,35 +1,17 @@
+/* i still didn't get it how the bufp gets to 1 */
+
 #include <stdio.h>
 #include <ctype.h>
-
 #define BUFSIZE 100
 
-int getch(void);
-void ungetch(int c); /* to drop the int part */
 
 int buf[BUFSIZE]; /* buffer for ungetch */
 int bufp = 0;     /* next free position in buf */
 
-int main(void)
-{
-	int i = 0;
-	char ch;
-	puts("Input an integer followed by a char:");
-	/* read chars until non digit or EOF */
-	while ((ch = getch()) != EOF && isdigit(ch))
-		i = 10 * i + ch - 48; /* convert ASCII into int value */
-
-		/* if non digit char was read, push it back into input buffer */
-		if (ch != EOF)
-			ungetch(ch);
-
-	printf("\n\ni = %d, next char in buffer = %c\n", i, getch());
-	return 0;
-}
-
-/* getch: get a (possibly pushed back) character */
 int getch(void)
 {
 	return (bufp > 0) ? buf[--bufp] : getchar();
+    printf("%d\n", bufp);
 }
 
 /* ungetch: push a character back onto the input */
@@ -39,4 +21,27 @@ void ungetch(int c)
 		printf("ungetch: too many characters \n");
 	else
 		buf[bufp++] = c;
+}
+
+int main(void)
+{
+	int i = 0;
+	char ch;
+	puts("input an integer followed by a char:");
+
+
+	/* read chars until non digit or EOF */
+	while ((ch = getch()) != EOF && isdigit(ch))
+		i = 10 * i + ch - 48;
+
+		if (ch != EOF)
+			ungetch(ch);
+
+	printf("i = %d, next char in buffer = %c\n", i, getch());
+    printf("%c\n", ch);
+
+    for (int i = 0; i<BUFSIZE; i++)
+        printf("%d", buf[i]);
+    printf("\n");
+	return 0;
 }
