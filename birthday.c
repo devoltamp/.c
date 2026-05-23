6d25338b1aab964c3b66f5ea4e7ffcbd26d3b46c
devoltamp <devparmar2306@gmail.com>
2026-05-23 14:14:57 +0530

    my 20th birthday

---
+++ birthday.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main(){
+    char b[] = "happy birthday to me!";
+    int i = 0;
+    while (b[i] != '\0'){
+        printf("%c", b[i]);
+        i++;
+        Sleep(50);
+    }
+    return 0;
+}
