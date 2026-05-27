6674d745eed05c3d46e995f00da571b37db21054
devoltamp <devparmar2306@gmail.com>
2026-05-27 14:46:07 +0530

    added addpoint

--- struct02.c
+++ struct02.c
@@ -21,6 +21,14 @@ struct point makepoint(int x, int y){
     return temp;
 }
 
+/* addpoint: that will add two points */
+struct point addpoint(struct point p1, struct point p2){
+    p1.x += p2.x;
+    p1.y += p2.y;
+    return p1;
+    /* instead of making an explicit temp the value is stored in the p1 */
+}
+
 int main(){
 
     struct rect screen;
@@ -30,9 +38,16 @@ int main(){
     /* making the rect. from 0,0 to the max of a & y */
     screen.pt1 = makepoint(0, 0);
     screen.pt2 = makepoint(XMAX, YMAX);
-    middle = makepoint((screen.pt1.x + screen.pt2.x) / 2 , (screen.pt1.y + screen.pt2.y) / 2);
 
+    puts("middle of two points:");
+    middle = makepoint((screen.pt1.x + screen.pt2.x) / 2 , (screen.pt1.y + screen.pt2.y) / 2);
     printf("%d, %d\n", middle.x, middle.y);
+
+    /* again reuse of the middle */
+    puts("adding of two points:");
+    middle = addpoint(screen.pt1, screen.pt2);
+    printf("%d, %d\n", middle.x, middle.y);
+
     return 0;
 }
 /* works like a charm with a little tweaks */
