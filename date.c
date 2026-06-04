ae5fea1c1a96f8441853cc95576a9433027b8e44
devoltamp <devparmar2306@gmail.com>
2026-06-04 13:41:51 +0530

    via the command execution

---
+++ date.c
@@ -0,0 +1,12 @@
+/* printing date via a sys. call*/
+#include <stdio.h>
+#include <windows.h>
+
+int main(){
+
+    system("cls");
+    system("date");
+    /* somehow the sys. is adding a little bit of extra */
+
+    return 0;
+}
