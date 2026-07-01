be9958abf41524e1951079bb7a5d5195e207deef
devoltamp <devparmar2306@gmail.com>
2026-07-02 00:18:10 +0530

    i think there's a problem -- but i could not find it

--- dsa/ll_doubly01.c
+++ dsa/ll_doubly01.c
@@ -13,16 +13,12 @@ struct node{
 };
 
 struct node *start = NULL;
-int main(){
-    clrscr();
-    return 0;
-}
 
 struct node *create_ll(struct node *start){
 
     struct node *nn, *ptr;
     int num;
-    printf("enter -1 to end");
+    printf("enter -1 to end\n");
     printf("enter the data: ");
     scanf("%d", &num);
 
@@ -31,26 +27,77 @@ struct node *create_ll(struct node *start){
         /* if only one time exicution */
         if (start == NULL){
             nn = (struct node *)malloc(sizeof(struct node));
-            nn->prev = NULL;
-            nn->data = num;
+            start = nn;
             nn->next = start;
-            nn = start;
+            nn->prev = start;
+            nn->data = num;
         }
         else{
             nn = (struct node *)malloc(sizeof(struct node));
             nn->data = num;
             ptr = start;        /* at the nn right now */
+            /* ptr at the last */
             while(ptr->next != start){
                 ptr = ptr->next;
             }
 
-            /* ptr at the last */
+            nn->next = start;
             nn->prev = ptr;
             ptr->next = nn;
-            nn->next = start;
             start->prev = nn;
+            ptr->next = start;
+            /* the main thing that's why the circular dll */
         }
         printf("enter another data: ");
         scanf("%d", &num);
     }
+}
+
+struct node *display(struct node *start){
+
+    struct node *ptr;
+    ptr = start;
+
+    do{
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+    while(ptr != start);
+
+    return start;
+}
+
+
+struct node *insert_beg(struct node *start){
+
+    struct node *ptr, *nn;
+    int num;
+    printf("enter data: ");
+    scanf("%d", &num);
+
+    nn = (struct node *)malloc(sizeof(struct node));
+    nn->data = num;
+    ptr = start;
+    while(ptr->next != start){
+        ptr = ptr->next;
+    }
+
+    ptr->next = nn;
+    nn->prev = ptr;
+    nn->next = start;
+    start->prev = nn;
+    start = nn;
+    /* nn is the start -- that's all */
+
+    return start;
+}
+
+
+int main(){
+
+    start = create_ll(start);
+    start = insert_beg(start);
+    start = display(start);
+
+    return 0;
 }
