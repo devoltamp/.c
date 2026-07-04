af2886d7b1d6503810663c2369a43f9a01e17ac7
devoltamp <devparmar2306@gmail.com>
2026-07-04 23:49:17 +0530

    made it fancy

--- dsa/stack01.c
+++ dsa/stack01.c
@@ -1,4 +1,7 @@
 /* linked rep. */
+/* just lookout for the overflow,
+underflow & the empty stacks */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -46,3 +49,61 @@ struct stack *pop(struct stack *top){
     }
     /* attached prog. from the push */
 }
+
+/* display */
+struct stack *display(struct stack *top){
+
+    struct stack *ptr;
+    ptr = top;
+    if (top == NULL){
+        puts("empty stack");
+    }
+    else{
+        printf("%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+    return top;
+}
+
+/* peek */
+int peek(struct stack *top){
+
+    return top->data;
+}
+
+int main(){
+
+    int val, option;
+    do{
+        puts("----- main menu -----");
+        puts("1. push");
+        puts("2. pop");
+        puts("3. peek");
+        puts("4. display");
+        puts("5. exit");
+
+        printf("enter #n: ");
+        scanf("%d", &option);
+        switch(option){
+
+            case 1:
+                puts("enter the #n to push: ");
+                scanf("%d", &val);
+                top = push(top, val);
+                break;
+            case 2:
+                top = pop(top);
+                break;
+            case 3:
+                val = peek(top);
+                break;
+            case 4:
+                top = display(top);
+                break;
+        };
+    }
+    while (option != 5);
+
+    return 0;
+}
+/* ptr is always the follow along */
