6b5eab44894b9fdec54391637fd9058c8f735827
devoltamp <devparmar2306@gmail.com>
2026-07-11 00:35:58 +0530

    added b

--- dsa/queue/multiple.c
+++ dsa/queue/multiple.c
@@ -51,14 +51,77 @@ void a_display(){
     }
 }
 
+void b_insert(){
+
+    int val;
+    printf("enter data: ");
+    scanf("%d", &val);
+
+    if (ra == rb - 1){
+        puts("overflow");
+    }
+    else{
+        /* only one time */
+        if (rb == MAX && fb == MAX){
+            rb = fb = MAX - 1;
+            queue[rb] = val;    /* indexing matters */
+        }
+        /* rest of the time */
+        else{
+            queue[--rb] = val;
+        }
+    }
+}
+
+int b_del(){
+
+    int val;
+    if (fb == MAX){
+        puts("full");
+        return -1;
+    }
+    else{
+        val = queue[fb];
+        fb--;
+        if (fb < rb){
+            fb = rb = MAX;
+        }
+        return val;
+    }
+}
+
+void b_display(){
+
+    if (fb == MAX){
+        puts("queue - B is empty");
+    }
+    else{
+        int i;
+        for (i = fb; i>= rb; i--){
+            printf("%d ", queue[i]);
+        }
+    }
+}
+
 int main(){
 
     int j = 0;
-    while(j < MAX){
+    while (j < MAX){
         a_insert();
         j++;
     }
     a_del();
     a_display();
+    printf("\n");
+
+    /* once all the queue is fill
+    it'll not accept answer from the rear que. B
+    -- that's pretty cool */
+    while (j < MAX){
+        b_insert();
+    }
+    b_del();
+    b_display();
+
     return 0;
 }
