ac2b7c0ac05e928fca941a2ae81cda458b8a2f6b
devoltamp <devparmar2306@gmail.com>
2026-07-25 19:58:47 +0530

    i didn't know that, main() could also be called

---
+++ dsa/tree/recursive.c
@@ -0,0 +1,8 @@
+/* https://www3.cs.stonybrook.edu/~cse230/CSE230_L04_Recursion.pdf */
+#include <stdio.h>
+
+int main(){
+    printf("the universe is never ending\n");
+    main();
+    return 0;
+}
