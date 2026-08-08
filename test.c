5f512e1327bf8b6777b185b375ebfdef568e05dd
devoltamp <devparmar2306@gmail.com>
2026-08-08 23:27:25 +0530

    idk

--- test.c
+++ test.c
@@ -1,12 +1,34 @@
+/* idk but thought why not make a simple prog. that can do it for me
+https://www.cs.yale.edu/homes/aspnes/pinewiki/C(2f)Randomization.html
+*/
 #include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* test - 1 */
+void _commit(){
+    srand(time(NULL));
+    char *phrases[] = {
+        "-",
+        "idk",
+        "meh",
+        "apn",
+        "check out the comments"
+        "get a life"
+        "please work"
+    };
+    int len = sizeof(phrases) / sizeof(phrases[0]);
+    int index = rand() % len;
+
+    char buf[100];
+    sprintf(buf, "git commit -m """);
+    // char s = 'c';
+    system("git commit -m ", """s""");
+}
 
 int main(){
-    
-    int c;
-    while (1)
-    {
-        printf("noice\n");
-    }
-    
+
+    _commit();
     return 0;
 }
